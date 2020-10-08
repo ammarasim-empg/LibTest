7289978b56ba5278f60987ca04320710e175052b
ammarasim <ammar.asim@zameen.com>
2020-10-08 15:34:18 +0500

    1

---
+++ testlibrary/src/main/java/com/example/testlibrary/T.java
@@ -0,0 +1,10 @@
+package com.example.testlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class T {
+    public static void s(Context c, String m) {
+        Toast.makeText(c, m, Toast.LENGTH_SHORT).show();
+    }
+}
